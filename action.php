1e6da0a9f7014e7b4883443efe941b693f1af6a4
ayman f. ali <ayman4swd@gmail.com>
2025-08-13 14:36:34 +0300

    feat: add result

--- action.php
+++ action.php
@@ -4,19 +4,22 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 
     $name = $_POST['name'];
     $color = $_POST['color'];
+    $encodedColor = urlencode($color);
 
-    if (isset($name) && !empty($name) &&
-        isset($color) && !empty($color)) {
-        
-         header('Location: result.php?' . $name . '+' . $color, true, 303);
+    if (
+        isset($name) && !empty($name) &&
+        isset($color) && !empty($color)
+    ) {
+
+        header('Location: result.php?name=' . $name . '&color=' . $encodedColor, true, 303);
     } else {
         header('Location: index.html');
         echo "failed ";
-        
+
         exit;
     }
 } else {
-    
+
     header('Location: index.html');
     echo "failed request ";
 
